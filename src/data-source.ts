6b54ba077242958c894bd029e1cc98bbc101ed08
Sourav <souravxox@gmail.com>
2025-04-04 00:31:52 +0530

    `Update database connection settings and configuration in data-source.ts`

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,20 +6,35 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DATABASE_HOST,
-    port: Number(process.env.DATABASE_PORT),
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    synchronize: true, 
-    logging: false, 
-    entities: ["src/entities/*.ts"],
-    subscribers: [],
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 5432,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    synchronize: true,
+    logging: false,
+    ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
     extra: {
-        ssl: true,
-      },
+      ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false
+    },
+    entities: ["dist/entities/**/*.js"]
+    // type: "postgres",
+    // host: process.env.DATABASE_HOST,
+    // port: Number(process.env.DATABASE_PORT),
+    // username: process.env.DATABASE_USER,
+    // password: process.env.DATABASE_PASSWORD,
+    // database: process.env.DATABASE_NAME,
+    // synchronize: true, 
+    // logging: false, 
+    // entities: ["src/entities/*.ts"],
+    // subscribers: [],
+    // extra: {
+    //     ssl: true,
+    //   },
 });
 
+
+  
 AppDataSource.initialize()
     .then(() => console.log("Database Connected Successfully"))
     .catch((error: any) => console.error("Database Connection Error: ", error));
